ab21c7333d5d29969f05b0e23a6b440f5308b015
Nasim Rishal V P <nasimrishal27@gmail.com>
2025-04-08 14:54:01 +0530

    [UPD] Multiple record selection on report updated

--- wizard/rental_lease_pdf_report.py
+++ wizard/rental_lease_pdf_report.py
@@ -12,10 +12,11 @@ class RentalLeasePdfReport(models.TransientModel):
     state = fields.Selection([("draft", "Draft"), ("to-approve", "To Approve"),
                               ("confirm", "Confirmed"), ("close", "Closed"), ("return", "Returned"),
                               ("expired", "Expired")])
-    tenant_id = fields.Many2one(comodel_name="res.partner", string="Tenant")
-    owner_id = fields.Many2one(comodel_name='res.partner', string="Owner")
+    tenant_ids = fields.Many2many(comodel_name="res.partner", string="Tenant")
+    owner_ids = fields.Many2many(comodel_name='res.partner', string="Owner",
+                                 relation="res_partner_rental_lease_pdf_report_owner_id_rel")
     property_type = fields.Selection([("rent", "Rent"), ("lease", "Lease")], string="Type")
-    property_id = fields.Many2one(comodel_name='property.property', string="Property")
+    property_ids = fields.Many2many(comodel_name='property.property', string="Property")
 
     def action_print_pdf(self):
         """Function for printing report"""
@@ -29,22 +30,18 @@ class RentalLeasePdfReport(models.TransientModel):
         if self.to_date:
             where_clause.append("date_end <= '%s'" % self.to_date)
         if self.state:
-            where_clause.append("state = '%s'" % self.state)
-        if self.tenant_id:
-            where_clause.append("tenant_id = %s" % self.tenant_id.id)
-        if self.owner_id:
-            where_clause.append("owner_id = %s" % self.owner_id.id)
+            where_clause.append("r.state = '%s'" % self.state)
+        if self.tenant_ids:
+            where_clause.append("tenant_id in (%s)" % str(self.tenant_ids.ids)[1:-1])
+        if self.owner_ids:
+            where_clause.append("owner_id in (%s)" % str(self.owner_ids.ids)[1:-1])
         if self.property_type:
             where_clause.append("property_type = '%s'" % self.property_type)
-        if self.property_id:
-            where_clause.append("property_id = %s" % self.property_id.id)
+        if self.property_ids:
+            where_clause.append("property_id in (%s)" % str(self.property_ids.ids)[1:-1])
         if where_clause:
             query += " WHERE " + " AND ".join(where_clause)
         self.env.cr.execute(query)
         report = self.env.cr.dictfetchall()
         data = {'report': report}
         return self.env.ref('property_management.action_rental_lease_request_report').report_action(None, data=data)
-
-    def action_back(self):
-        """cancel button"""
-        return {'type': 'ir.actions.act_window_close'}
